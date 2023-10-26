bce7c72aa95ebe2542cbe8f82f0e5c1064645fd5
helmy <106034331+helmii18@users.noreply.github.com>
2023-10-26 22:23:07 +0300

    Add files via upload

---
+++ second.java
@@ -0,0 +1,11 @@
+package com.example.f1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class second extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+    }
+}
